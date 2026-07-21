dee05dc02ce54e0ec6efc852126f42949546ac1a
SHAFIN <s8392118@gmail.com>
2026-07-21 13:39:35 +0600

    Update config.php

--- Library Management System/data/config.php
+++ Library Management System/data/config.php
@@ -2,9 +2,9 @@
 session_start();
 
 $servername = "sql107.byetcluster.com:3306"; 
-$username = "if0_40979562";             
-$password = "XaqSrFTid8E57SV"; 
-$dbname = "if0_40979562_BIST";
+$username = "admin";             
+$password = "admin"; 
+$dbname = "LMS-BIST";
 
 if (!isset($_COOKIE['db_auth'])) {
     $conn = new mysqli($servername, $username, $password, $dbname);
